fdf49b8ba32224d754e14c61c88fdb731f70a3e0
mugarate12 <serjumano17@gmail.com>
2020-10-15 11:04:06 -0300

    fix bug in switch of commands

--- src/commands/general.ts
+++ src/commands/general.ts
@@ -5,20 +5,23 @@ export default (client: Client, channel: string, comand: string) => {
   switch (comand) {
     case types.github:
       client.say(
-        channel, 
-        'github do criador do Bot: https://github.com/mugarate12')
-    
+        channel,
+        'github do criador do Bot: https://github.com/mugarate12'
+      )
+      return
     case types.commands:
       let commandsList = ''
       const typesKeys = Object.keys(types)
       
       typesKeys.forEach(keyName => {
-        commandsList += `!${keyName} |`
+        commandsList += `!${keyName} | `
       })
 
       client.say(
         channel,
         commandsList
       )
+
+      return
   }
 }
--- src/commands/lol.ts
+++ src/commands/lol.ts
@@ -6,12 +6,17 @@ export default (client: Client, channel: string, comand: string) => {
   switch (comand) {
     case types.duo:
       client.say(
-        channel, 
-        'Te dou o mundo e quantas kills quiser, meu amor!')
-    
+        channel,
+        'Te dou o mundo e quantas kills quiser, meu amor!'
+      )
+
+      return
     case types.alves:
       client.say(
         channel,
-        `O Alves já morreu ${Math.round(Math.random() * 20)} vezes enquanto você digitava`)
+        `O Alves já morreu ${Math.round(Math.random() * 20)} vezes enquanto você digitava`
+      )
+
+      return
   }
 }
